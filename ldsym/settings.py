b9e4535f5c110c24f64dc8d2df45bc8dd599c504
Terra Codes <kehindeowolabi004@gmail.com>
2025-01-25 02:27:30 +0100

    Update settings.py

--- ldsym/settings.py
+++ ldsym/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-cyyd32yinx8yz8qifzt*r8x11n)vi7z7sss*k@&(xh)(9vuxn*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition

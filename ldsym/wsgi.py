f9f79bdf9f4bbbca54cf84151b06e9e1957ae483
Terra Codes <kehindeowolabi004@gmail.com>
2025-01-26 22:56:42 +0100

    Update wsgi.py

--- ldsym/wsgi.py
+++ ldsym/wsgi.py
@@ -10,6 +10,6 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ldsym.settings')
 
 app = get_wsgi_application()

69496b7d3c8a1aa2dbed01ba65e30da3b0c687d7
Terra Codes <kehindeowolabi004@gmail.com>
2025-03-18 22:01:43 +0100

    Update wsgi.py

--- ldsym/wsgi.py
+++ ldsym/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ldsym.settings')
 
 application = get_wsgi_application()
+# api/wsgi.py
+app = get_wsgi_application()
